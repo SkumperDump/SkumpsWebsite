b3243e3d33ed0645495585ce6fa2dc7a78d2dca6
Scott Wallace <scottytridude@gmail.com>
2023-03-17 21:26:32 -0700

    added styling to index.html

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from flask import (Flask, render_template)
 
 nav_list = ['homepage', 'projects', 'blog']
 
+contactInfo = utils.open_file("app/static/md/contact.md")
+contactInfo = markdown.markdown(contactInfo)
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -11,13 +14,13 @@ def homepage():
     # need to use {{FOO | safe}} in jinja template to prevent auto-escaping
     content = utils.open_file("app/static/md/index.md")
     content = markdown.markdown(content)
-    return render_template("index.html", navigation=nav_list, content=content)
+    return render_template("index.html", navigation=nav_list, content=content, contactInfo=contactInfo)
 
 @app.route('/projects')
 def projects():
     content = utils.open_file("app/static/md/projects.md")
     content = markdown.markdown(content)
-    return render_template("projects.html", navigation=nav_list, content=content)
+    return render_template("projects.html", navigation=nav_list, content=content, contactInfo=contactInfo)
 
 @app.route('/blog')
 def blog():
---
+++ app/static/md/contact.md
@@ -0,0 +1,6 @@
+* Philip Scott Wallace (he/him)
+* 3414 NW Bryce Canyon Ln
+* Bend, OR 97703
+* Call me at: [1-210-845-3963](tel:+12108453963)
+* Email me at: [philipswallace1@gmail.com](mailto:philipswallace1@gmail.com)
+* Find me on github: [https://github.com/SkumperDump](https://github.com/SkumperDump)
