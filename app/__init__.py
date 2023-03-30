72ff68a54ebd19305d8171f07a25e55b409cc960
Scott Wallace <scottytridude@gmail.com>
2023-03-30 16:24:23 -0700

    fixed bug where relative path was setup incorrectly

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask import (Flask, render_template)
 from pathlib import Path
 
 nav_list = ['homepage', 'projects', 'blog', 'books']
-markdownDir = str(Path.home()) + "/SkumpsWebsite/app/static/md/{}.md"
+markdownDir = str(Path.home())
+markdownDir += "/SkumpsWebsite/app/static/md/{}"
 
 # turns markdown file into string of html
-contactInfo = utils.mdToHtml(markdownDir.format("contact"))
+contactInfo = utils.mdToHtml(markdownDir.format("contact.md"))
 
 app = Flask(__name__)
 
@@ -14,20 +15,20 @@ app = Flask(__name__)
 def homepage():
     # markdown -> html
     # need to use {{FOO | safe}} in jinja template to prevent auto-escaping
-    content = utils.mdToHtml(markdownDir.format("index"))
+    content = utils.mdToHtml(markdownDir.format("index.md"))
     return render_template("index.html", navigation=nav_list, content=content, contactInfo=contactInfo)
 
 @app.route('/projects')
 def projects():
-    content = utils.mdToHtml(markdownDir.format("projects"))
+    content = utils.mdToHtml(markdownDir.format("projects.md"))
     return render_template("projects.html", navigation=nav_list, content=content, contactInfo=contactInfo)
 
 @app.route('/blog')
 def blog():
-    content = utils.mdToHtml(markdownDir.format("blog"))
+    content = utils.mdToHtml(markdownDir.format("blog.md"))
     return render_template("blog.html", navigation=nav_list, content=content, contactInfo=contactInfo)
 
 @app.route('/books')
 def books():
-    content = utils.mdToHtml(markdownDir.format("books"))
+    content = utils.mdToHtml(markdownDir.format("books.md"))
     return render_template("books.html", navigation=nav_list, content=content, contactInfo=contactInfo)
